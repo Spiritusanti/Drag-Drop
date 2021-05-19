e844922d447f705f987cb21f324be4266d6b240a
Spiritusanti <mccrackenjacob@gmail.com>
2021-05-19 09:50:37 -0700

    updating all files

---
+++ src/models/drag-drop.ts
@@ -0,0 +1,14 @@
+// Drag and Drop Interfaces
+namespace App {
+    export interface Draggable {
+        DragStartHandler(event: DragEvent): void;
+        DragEndHandler(event: DragEvent): void;
+    }
+    
+    export interface DragTarget {
+        DragOverHandler(event: DragEvent): void;
+        DropHandler(event: DragEvent): void;
+        DropLeaveHandler(event: DragEvent): void;
+    }
+}
+
